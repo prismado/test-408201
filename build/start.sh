95b3738f3c80f70433b2d74202352fa5bff25275
Reto S. <mrs.zbook@ch2.be>
2024-08-20 15:31:53 +0200

    tbd 15:31 Uhr

---
+++ build/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# -- /usr/local/bin/start.sh
+
+ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+echo "Europe/Zurich" | tee /etc/timezone
+dpkg-reconfigure --frontend noninteractive tzdata
+
+/etc/init.d/mariadb start
+
+service cron start
+
+echo -n `date` > /root/readme_407251.txt
+echo " CPAN install: HTML::Mason::ApacheHandler" >> /root/readme_407251.txt
+
+(echo yes | perl -MCPAN -e "install HTML::Mason::ApacheHandler") &
+PID=$!
+
+echo -n `date` >> /root/readme_407251.txt
+echo " Installing (PID $PID) ... wait for 'Installed' this may take 5 minutes ..." >> /root/readme_407251.txt
+
+wait $PID
+
+echo -n `date`    >> /root/readme_407251.txt
+echo " Installed" >> /root/readme_407251.txt
+
+mkdir  /etc/apache2/mason && chown www-data /etc/apache2/mason
+
+apache2ctl -D FOREGROUND
